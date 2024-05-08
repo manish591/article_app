2a11b0a791c2d2a75b97f7c7e8a25a7a1a0d6bcd
Manish Devrani <manishdevrani777@gmail.com>
2024-05-09 02:48:54 +0530

    CONFIG: added testing setup

---
+++ backend/src/server.ts
@@ -0,0 +1,5 @@
+export default function sum(a: number, b: number) {
+  return a + b;
+}
+
+console.log("the sum is: ", sum(2, 4));
---
+++ backend/test/sum.test.ts
@@ -0,0 +1,11 @@
+import sum from "../src/server";
+
+describe('sum method', function() {
+  it("should return the sum of 4 and 5 to be 9", function() {
+    const a = 5;
+    const b = 10;
+
+    const result = sum(a, b);
+    expect(result).toBe(15);
+  });
+})
