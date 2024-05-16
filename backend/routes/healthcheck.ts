c95c5ecb008d12002292a569dbfc860979c9eb9e
Manish Devrani <manishdevrani777@gmail.com>
2024-05-17 00:17:41 +0530

    update healthcheck route slug

--- backend/routes/healthcheck.ts
+++ backend/routes/healthcheck.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/healthcheck", function(req: Request, res: Response) {
+router.get("/", function(req: Request, res: Response) {
   res.json({
     uptime: new Date(),
   });
