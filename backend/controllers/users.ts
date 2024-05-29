cbfd800ea13fab1bacdb0722d052f091c5211163
Manish Devrani <manishdevrani777@gmail.com>
2024-05-30 00:56:28 +0530

    added find user by username api

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import logger from "../utils/logger";
 import { sanitizeData } from "../utils/sanitizeUser";
+
+const prisma = new PrismaClient();
 
 async function getSelfDetails(req: Request, res: Response) {
   try {
@@ -28,8 +31,57 @@ async function getSelfDetails(req: Request, res: Response) {
   }
 }
 
-async function getUserByUsername() {
+async function getUserByUsername(req: Request, res: Response) {
+  try {
+    const username = req.params.username;
 
+    if(!username) {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "not found",
+        data: {},
+        meta: {}
+      });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        username
+      }
+    });
+
+    if(!user) {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "not found",
+        data: {},
+        meta: {}
+      });
+    }
+
+    const sanitizedUserData = sanitizeData(user, ["email", "password"]);
+
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "successfully returns user",
+      data: {
+        ...sanitizedUserData
+      },
+      meta: {},
+    });
+  } catch(err) {
+    logger.error("Internal server error occured", err);
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal server error occured",
+      data: {},
+      meta: {}
+    });
+  }
 }
 
 async function updateSelfDetails() {
--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -4,16 +4,8 @@ import usersController from "../controllers/users";
 
 const router = Router();
 
-/**
- * 1. get self details, it will return details of the user that is logged in. we will use this to populate whether user is logged
- * in in the frontend or not.
- * 2. Get user by username. this will be used to get the profile of the user
- * 3. update profile, we will have an settings page that will allow us to update variouse user details from time to time
- * 4. Delete profile
- */
-
 router.get("/self", authenticate, usersController.getSelfDetails);
-router.get("/username", authenticate);
+router.get("/:username", authenticate, usersController.getUserByUsername);
 router.patch("/self", authenticate);
 router.delete("/self", authenticate);
 
