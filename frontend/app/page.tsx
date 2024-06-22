c39c5e2f6f0f48deadcfa73aa213751ac500b1bf
Manish Devrani <manishdevrani777@gmail.com>
2024-06-22 23:15:31 +0530

    frontend initialized

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Create meaningful blogs with us</h1>
+    </main>
+  );
+}
